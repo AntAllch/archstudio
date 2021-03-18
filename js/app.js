df4db58ebe2de59f0c0a42806a36a296f3f9b701
AntAllch <anthonyallchorne@gmail.com>
2021-03-18 22:49:51 +0000

    Part done Welcome

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ let hamburger = $(".hamburger").on("click", () => {
     $(".mobile-nav").addClass("open")
     $(".hamburger").css("display","none");
     $(".close").css("display","block");
+    $(".mobile").css("position","fixed");
 
     // //each mobile nav link fades in 1 by 1
     let mobNavLink = $(".mobile-nav-link");
@@ -23,6 +24,7 @@ let close = $(".close").on("click", () => {
     $(".mobile-nav").removeClass("open");
     $(".hamburger").css("display","block");
     $(".close").css("display","none");
+    $(".mobile").css("position","initial");
 
     //mobile nav links have "appear" class removed
     let mobNavLink = $(".mobile-nav-link");
