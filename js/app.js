62dbbe6f47c7399159009bf42cd4deb74f7470ae
AntAllch <anthonyallchorne@gmail.com>
2021-03-25 22:19:54 +0000

    Added in some animation

--- js/app.js
+++ js/app.js
@@ -44,8 +44,68 @@ let close = $(".close").on("click", () => {
 
 //-------------------------- Start of Intersection Observer --------------------------//
 
+//Home Hero Variables
+const nav = document.querySelector("nav");
+const indexHero = document.querySelector("header");
+const heroText = document.querySelector(".hero-textcontent");
+const pageTags = document.querySelector(".page-tags");
+
+//Home Welcome Variables
+const welcome = document.querySelector(".welcome");
+const welcomeText = document.querySelector(".welcome-textcontent");
+const welcomeImg = document.querySelector(".welcome-image");
+
+//Intersectional Observers Options
+const indexHeroOptions = {};
+const welcomeOptions = {
+    threshold: 0.5
+};
 
 
+
+
+//Index Hero Observer
+const indexHeroObserver = new IntersectionObserver(function(entries, indexHeroObserver) {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+
+            setTimeout(() => { //Wait 2 seconds before running this function
+                nav.classList.add("fade-in");
+                pageTags.classList.add("fade-in");
+            }, 1900)
+
+            setTimeout(() =>{
+                heroText.classList.add("fade-in");
+            }, 2100)
+
+            indexHeroObserver.unobserve(entry.target)
+        } 
+        console.log(entry.target);
+    });
+}, indexHeroOptions);
+
+indexHeroObserver.observe(indexHero);
+
+
+
+
+
+//Welcome Section Observer
+const welcomeObserver = new IntersectionObserver(function(entries, welcomeObserver) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting){ 
+            return;
+        } else {
+            welcomeText.classList.add("fade-in");
+            welcomeImg.classList.add("fade-in");
+            welcomeObserver.unobserve(entry.target)
+        } 
+        
+        console.log(entry.target);
+    });
+}, welcomeOptions);
+
+welcomeObserver.observe(welcome);
 //--------------------------- End of Intersection Observer ---------------------------//
 
 
--- js/gsap.js
+++ js/gsap.js
@@ -6,11 +6,6 @@ tl.to(".text", {y: 0, duration: 1, stagger: 0.3});
 tl.fromTo(".intro", {y: "0"}, {y: "-100%", duration: 1}, "-=0.3" );
 tl.fromTo(".slider", {y: "0"}, {y: "-100%", duration: 1}, "-=0.9" );
 
-tl.to(".hero-textcontent", {opacity: "1", duration: 1.5 }, "-=0.5");
-// tl.to(".hero-textcontent", {x: "0%", duration: 1.5 });
-tl.to("nav", {opacity: "1", duration: 1.5 }, "-=1");
-tl.to(".page-tags", {opacity: "1", duration: 1.5 }, "-=1.5");
-
 
 //------------------------------ End of GSAP Animations ------------------------------//
 
